b229854ae7643dbc24732cc271ef1ab0e0992a1b
Laura Kressin <laurakressin@gmail.com>
2015-09-16 17:03:20 -0500

    Initial Commit

---
+++ scripts.js
@@ -0,0 +1,35 @@
+$(document).ready(function() {
+	var numOfClicks = 0;
+
+	$('button').on('click', function(){
+			$("body").append("<div class"+numOfClicks+">"+'Line #'+numOfClicks+"</div>");
+			$("."numOfClicks").append("<button>Color</button>"));
+			$("."numOfClicks").append("<button>Remove</button>");
+			// $('button').addClass.attr('continueCount')
+			return numOfClicks += 1;
+	});
+	// $('continueCount').on('click', function(){
+	// 	numOfClicks += 1;
+	// });
+	
+	$("."numOfClicks").append("<button>Color</button>");
+	$("."numOfClicks").append("<button>Remove</button>");
+
+
+});
+//assign classes for colors
+var modulus = numOfClicks%9
+switch(modulus){
+	case 0:
+		$
+	case 9:
+	case 8:
+	case 7:
+	case 6:
+	case 5:
+	case 4:
+	case 3:
+	case 2:
+	case 1:
+	default:
+}
